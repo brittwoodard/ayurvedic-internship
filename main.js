3845aa50ee92113bca738534e6ecd9c877c56575
brittwoodard <96916819+brittwoodard@users.noreply.github.com>
2024-01-17 11:02:15 +0100

    mobile menu home

---
+++ main.js
@@ -0,0 +1,9 @@
+// White Mobile Menu
+$(document).ready(function () {
+    $(document).ready(function () {
+      $(".mobile-button").click(function () {
+        $(".mobile-button").toggleClass("active");
+        $(".mobile-menu").toggleClass("active");
+      });
+    });
+  });
