b0aa5d1e6393cd926f576e8763fcf5f5e09bd6b1
brittwoodard <96916819+brittwoodard@users.noreply.github.com>
2024-01-17 13:19:47 +0100

    mobile button complete

--- _site/main.js
+++ _site/main.js
@@ -1,9 +1,21 @@
-// White Mobile Menu
+// Light Mobile Menu
+// A $( document ).ready() block.
 $(document).ready(function () {
-    $(document).ready(function () {
-      $(".mobile-button").click(function () {
-        $(".mobile-button").toggleClass("active");
-        $(".mobile-menu").toggleClass("active");
-      });
+  $(document).ready(function () {
+    $(".mobile-button").click(function () {
+      $(".mobile-button").toggleClass("active");
+      $(".mobile-menu").toggleClass("active");
     });
   });
+});
+
+// Dark Mobile Menu
+// A $( document ).ready() block.
+$(document).ready(function () {
+  $(document).ready(function () {
+    $(".mobile-button-dark").click(function () {
+      $(".mobile-button-dark").toggleClass("active");
+      $(".mobile-menu").toggleClass("active");
+    });
+  });
+});
